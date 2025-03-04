8d76d34e1815f7ff3b6b74e25100da65d23f7339
Max Penet <mpenet@s-exp.com>
2025-03-04 12:34:34 +0100

    add 4.2.* options

--- src/s_exp/hirundo/options.clj
+++ src/s_exp/hirundo/options.clj
@@ -1,5 +1,6 @@
 (ns s-exp.hirundo.options
-  (:import (io.helidon.common.socket SocketOptions$Builder)
+  (:import (io.helidon.common.concurrency.limits Limit)
+           (io.helidon.common.socket SocketOptions$Builder)
            (io.helidon.common.tls Tls)
            (io.helidon.webserver WebServerConfig$Builder)
            (java.time Duration)))
@@ -23,9 +24,37 @@
   [^WebServerConfig$Builder builder _ backlog _]
   (.backlog builder (int backlog)))
 
+(defmethod set-server-option! :idle-connection-timeout
+  [^WebServerConfig$Builder builder _ idle-connection-timeout-ms _]
+  (.idleConnectionTimeout builder (Duration/ofMillis idle-connection-timeout-ms)))
+
+(defmethod set-server-option! :idle-connection-period
+  [^WebServerConfig$Builder builder _ idle-connection-period-ms _]
+  (.idleConnectionPeriod builder (Duration/ofMillis idle-connection-period-ms)))
+
 (defmethod set-server-option! :max-payload-size
   [^WebServerConfig$Builder builder _ max-payload-size _]
   (.maxPayloadSize builder (long max-payload-size)))
+
+(defmethod set-server-option! :max-tcp-connections
+  [^WebServerConfig$Builder builder _ max-tcp-connections _]
+  (.maxTcpConnections builder (int max-tcp-connections)))
+
+(defmethod set-server-option! :concurrency-limit
+  [^WebServerConfig$Builder builder _ ^Limit concurrency-limit _]
+  (.concurrencyLimit builder concurrency-limit))
+
+(defmethod set-server-option! :max-concurrent-requests
+  [^WebServerConfig$Builder builder _ max-concurrent-requests _]
+  (.maxConcurrentRequests builder (long max-concurrent-requests)))
+
+(defmethod set-server-option! :max-in-memory-entity
+  [^WebServerConfig$Builder builder _ max-in-memory-entity _]
+  (.maxInMemoryEntity builder (int max-in-memory-entity)))
+
+(defmethod set-server-option! :smart-async-writes
+  [^WebServerConfig$Builder builder _ smart-async-writes _]
+  (.smartAsyncWrites builder (boolean smart-async-writes)))
 
 (defmethod set-server-option! :write-queue-length
   [^WebServerConfig$Builder builder _ write-queue-length _]
@@ -77,5 +106,3 @@
 (defmethod set-server-option! :tls
   [^WebServerConfig$Builder builder _ tls-config _]
   (doto builder (.tls ^Tls tls-config)))
-
-
