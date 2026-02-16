4cf0847934d3401f972d71f5f7a2eaf1c637f92d
Max Penet <mpenet@s-exp.com>
2026-02-16 21:06:32 +0100

    fix(response): Do nothing when response is nil
    Guard on the presence of the response map instead of checking
    bytesWritten.  This prevents attempting to set headers/status/body
    when no response is provided.

--- src/s_exp/hirundo/http/response.clj
+++ src/s_exp/hirundo/http/response.clj
@@ -31,7 +31,7 @@
 
 (defn set-response!
   [^ServerResponse server-response {:as response :keys [body headers status]}]
-  (when (zero? (.bytesWritten server-response))
+  (when response
     (set-headers! server-response headers)
     (set-status! server-response status)
     (write-body! server-response response body)))
